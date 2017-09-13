99f8e730e51ba45e75b92017dcf8f0af8bcfe343
Jeon <junbs95@gmail.com>
2017-09-14 00:10:35 +0900

    9_14

--- RRT_star/bicycle_revolving/bicycle_revolving_door_RRT.m
+++ RRT_star/bicycle_revolving/bicycle_revolving_door_RRT.m
@@ -21,7 +21,7 @@ end
 
 % Drawing 
 figure()
-for i=1:length(ts)
+for i=1:length(xs)
     plot([xs(1,i) xs(1,i)+L*cos(xs(3,i))],[xs(2,i) xs(2,i)+L*sin(xs(3,i))],'r-')
     plot(xs(1,i),xs(2,i) ,'ko')
 
@@ -36,6 +36,41 @@ obs_test=obstacle2(SE2([2 1],pi/4),[1 0.5]);
 obs_test.plot
 obs_test.isobs([3 2;2.5 1.5]')
 
+
+%% 3. Bicycle RRT*
+% (1) steering test
+%  state = (x,y,theta,t) input=(linear_vel, steering angle)
+
+s1=[0 0 0 0]; s2=[ 3 3 pi/4 2];
+d=norm(s1(1:2)-s2(1:2)); delt=s2(4)-s1(4);
+
+vs=d/delt*[0.5 1 1.5]; steer_angs=linspace(-pi/3,pi/3,8);
+vmin=0; steer_angs_min=0; cost_min=100;
+
+for v=vs
+    for steer_ang=steer_angs
+        x=s1(1:3)'; u=[v steer_ang]';
+        xs=[]; ts=[];
+        for t=0:dt:delt/2
+            x_next=RK(x,u,dt,@bicycle_dynamics);
+            xs=[xs x_next];
+            x=x_next;
+        end
+        cost=norm(x-s2(1:3));
+        if cost<cost_min
+           cost_min=cost;
+           xs_min=xs;
+        end
+    end
+end
+
+%(2) plain simulation - without any obstacle 
+
+        
+        
+        
+
+       
 
 
 
--- RRT_star/bicycle_revolving/bicycle_steering.m
+++ RRT_star/bicycle_revolving/bicycle_steering.m
@@ -1,5 +1,33 @@
-function x_new=bicycle_steering(x_near,x_rand)
-% This fucntion is bicycle RRT 
+function [x_new,u]=bicycle_steering(x_near,x_rand)
+% This fucntion is for bicycle RRT 
+% steering function 
+% Issue: delt/2
+dt=0.1; % integration 
+s1=x_near; s2=x_rand;
+d=norm(s1(1:2)-s2(1:2)); delt=s2(4)-s1(4); steer_time=delt/2;
 
+vs=d/delt*[0.5 1 1.5]; steer_ang=linspace(-pi/3,pi/3,8);
+vmin=0; steer_ang_min=0; cost_min=100;
 
+for v=vs
+    for steer_ang=steer_ang
+        x=s1(1:3)'; u=[v steer_ang]';
+        xs=[]; ts=[];
+        for t=0:dt:steer_time
+            x_next=RK(x,u,dt,@bicycle_dynamics);
+            xs=[xs x_next];
+            x=x_next;
+        end
+        cost=norm(x-s2(1:3));
+        if cost<cost_min
+           cost_min=cost;
+           xs_min=xs; vmin=v; steer_ang_min=steer_ang;
+        end
+    end
 end
+
+x_new=[x ; (s1(4)+steer_time)]; u=[vmin steer_ang_min];
+end
+       
+
+
