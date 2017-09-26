15b9fe08256a7f05110b1c2e05a5d3ff85d91a84
icsl-Jeon <junbs95@gmail.com>
2017-09-26 16:26:48 +0900

    9_26
    4quad RRT

--- RRT_star/4quadrotor/problem.m
+++ RRT_star/4quadrotor/problem.m
@@ -17,7 +17,7 @@ classdef problem
         obj.dim=dim;
         obj.range=range;
         obj.obslist=obslist;
-        obj.N=N;
+        obj.N=N;   % Npoint generation number 
         end
         
         function mapplot(problem)
@@ -32,6 +32,7 @@ classdef problem
             hold off
             
         end
+        
          
         function isobs=isobs1(problem,x)
             % is a state x in obs?
@@ -41,13 +42,33 @@ classdef problem
             % This section could be changed depending on problem 
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             
-            % State x= [ q_load q1 q2 q3 q4]
+            % State x= [ q_load alpha1 beta1 alpha2 beta2 alpha3 beta3 alpha4 beta4]
             Npnts=Npoint_gen(x(1:6),problem.rl,problem.N); 
             
             corners=Npoint_gen(x(1:6),problem.rl,4); 
             
-            q1=corners(:,1)+[problem.l*sin(x(7))]
+            q1=corners(:,1)+[problem.l*sin(x(7))*cos(x(8)) problem.l*sin(x(7))*sin(x(8)) problem.l*cos(x(7))]; % xyz of quadrotor1 
+            q1=[q1; zeros(3,1)];
             
+            
+            Npnts=[Npnts Npoint_gen(q1,problem.rq,problem.N)]; 
+            
+            q2=corners(:,2)+[problem.l*sin(x(9))*cos(x(10)) problem.l*sin(x(9))*sin(x(10)) problem.l*cos(x(9))]; % xyz of quadrotor2
+            q2=[q2; zeros(3,1)];
+            
+            Npnts=[Npnts Npoint_gen(q2,problem.rq,problem.N)]; 
+
+            
+            q3=corners(:,3)+[problem.l*sin(x(11))*cos(x(12)) problem.l*sin(x11))*sin(x(12)) problem.l*cos(x(11))]; % xyz of quadrotor3
+            q3=[q3 ; zeros(3,1)];
+
+            Npnts=[Npnts Npoint_gen(q3,problem.rq,problem.N)]; 
+            
+            q4=corners(:,4)+[problem.l*sin(x(13))*cos(x(14)) problem.l*sin(x(13))*sin(x(14)) problem.l*cos(x(13))]; % xyz of quadrotor4
+            q4=[q4; zeros(3,1)];
+                              
+            Npnts=[Npnts Npoint_gen(q4,problem.rq,problem.N)]; 
+           
             
             
             for i=1:nobs % for all the obstacles 
